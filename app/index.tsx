7f28728fedea5693d0c2a3a6dcaa479b7f2c3d7a
Jacob Clark <jacob.clark@UK-JCLARK.local>
2024-05-23 10:15:35 +0100

    add location header

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
+import { Header } from "@/components/Header";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Header location="City of London, England" />
   );
 }
---
+++ components/FocusedView.tsx
@@ -0,0 +1,17 @@
+ import { View, Text, StyleSheet } from 'react-native';
+
+ export function FocusedView({
+  value,
+  rating,
+  variable
+ }: {
+  value: number,
+  rating: string,
+  variable: string
+ }) {
+  return (
+    <View>
+      
+    </View>
+  );
+ }
