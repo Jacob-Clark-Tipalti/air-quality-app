b672721381b4cc1be371968fd611327f85a99914
Mia Quinn <mia.quinn@tipalti.com>
2024-05-24 14:06:50 +0100

    feat: api data is now dispayed on the ui

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,53 @@ import { FocusedView } from "@/components/FocusedView";
 import { Header } from "@/components/Header";
 import { VariableItem } from "@/components/VariableItem";
 import { Text, ScrollView, StyleSheet } from "react-native";
-import App from './App';
+import * as Location from 'expo-location';
+import { useEffect, useState } from "react";
+import { AirData } from "./airData";
+import React from "react";
+import axios from "axios";
 
 export default function Index() {
+  const [location, setLocation] = useState<Location.LocationObjectCoords|null>(null);
+  const [latitude, setLatitude] = useState<number>();
+  const [longitude, setLongitude] = useState<number>();
+  const [airData, setAirData] = useState<AirData|null>(null);
+
+  const fetchAirData = async(): Promise<AirData> => {
+    const url = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&current=european_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,uv_index,grass_pollen&forecast_days=3`;
+    const response = await axios.get<AirData>(url);
+    console.log(response)
+    return response.data;
+  }
+
+  const fetchLocation = async() =>{
+    let{status} = await Location.requestForegroundPermissionsAsync();
+    if(status !== 'granted'){
+        console.log('Permission to access location was denied');
+        return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location.coords);
+    setLatitude(location.coords.latitude);
+    setLongitude(location.coords.longitude);
+  }
+
+  useEffect(() => {
+    const getAllData = async () => {
+      fetchLocation();
+      const data = await Promise.resolve(fetchAirData());
+      setAirData(data);
+    }
+    getAllData();
+  }, []);
+
   return (
     <>
-      <App />
       <Header location="City of London, England" />
       <ScrollView contentContainerStyle={styles.view}>
-        <FocusedView value={20} rating="Fair" name="Air Quality" />
-        <VariableItem value={11.4} rating='Medium' name='NO2' />
+        <FocusedView value={airData?.current?.european_aqi} rating="Fair" name="Air Quality" />
+        <VariableItem value={airData?.current?.nitrogen_dioxide} rating='Medium' name='NO2' />
+        {/* <Text>{latitude}</Text> */}
       </ScrollView>
     </>
   );
