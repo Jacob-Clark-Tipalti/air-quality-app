051db8d9278cafc067a85a2828577a32086fa4b7
Mia Quinn <mia.quinn@tipalti.com>
2024-05-30 13:17:22 +0100

    feat: fixed scrolling issue

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { FocusedView } from "@/components/FocusedView";
 import { Header } from "@/components/Header";
 import { VariableItem } from "@/components/VariableItem";
-import { Text, ScrollView, StyleSheet } from "react-native";
+import { Text, ScrollView, StyleSheet, View } from "react-native";
 import { useEffect, useState } from "react";
 import { AirData } from "./airData";
 import React from "react";
@@ -36,20 +36,27 @@ export default function Index() {
       </Text>
 
       <ScrollView contentContainerStyle={styles.view}>
+      <View style={styles.scrollStyle}>
         <FocusedView value={airData?.current?.european_aqi} rating={airQualityRating("european_aqi", airData?.current?.european_aqi)} name="Air Quality" />
         <VariableItem value={airData?.current?.nitrogen_dioxide} rating={airQualityRating("nitrogen_dioxide", airData?.current?.nitrogen_dioxide)} name='NO2' />
         <VariableItem value={airData?.current?.pm10} rating={airQualityRating("pm10", airData?.current?.pm10)} name='PM10' />
         <VariableItem value={airData?.current?.pm2_5} rating={airQualityRating("pm2_5", airData?.current?.pm2_5)} name='PM2.5' />
         <VariableItem value={airData?.current?.sulphur_dioxide} rating={airQualityRating("sulphur_dioxide", airData?.current?.sulphur_dioxide)} name='SO2' />
         <VariableItem value={airData?.current?.ozone} rating={airQualityRating("ozone", airData?.current?.ozone)}name='03' />
+        <VariableItem value={airData?.current?.uv_index} rating={airQualityRating("uv_index", airData?.current?.uv_index)}name='UV' />
+      </View>
       </ScrollView>
+      
     </>
   );
 }
 
 const styles = StyleSheet.create({
   view: {
-    alignItems: 'center',
     top: 150,
+  }, 
+  scrollStyle: {
+    paddingBottom: '50%',
+    alignItems: 'center',
   }
 });
